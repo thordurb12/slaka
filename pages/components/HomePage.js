4f086ddb17c877b58e8c5f1b7284a09861c086f3
thordurb12 <thordur@gangverk.is>
2019-11-03 16:06:54 +0100

    Stuff

---
+++ pages/components/HomePage.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+const HomePage = (props) => {
+    return (
+        <React.Fragment>
+            {
+                <div>
+                    hdfg
+                    </div>
+            }
+        </React.Fragment>
+    )
+}
+
+export default HomePage;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Head from '../components/head'
 import Nav from '../components/nav'
 import Prismic from 'prismic-javascript'
 import { Date, RichText } from 'prismic-reactjs'
+import HomePage from './components/HomePage';
+
 
 const apiEndpoint = 'https://salka.cdn.prismic.io/api/v2'
 const accessToken = '' // This is where you would add your access token for a Private repository
@@ -27,25 +29,20 @@ const Home = () => {
     fetchData()
   }, [])
 
-  const linkResolver = (doc) => {
-    // Pretty URLs for known types
-    if (doc.type === 'blog') return `/post/${doc.uid}`
-    if (doc.type === 'page') return `/${doc.uid}`
-    // Fallback for other types, in case new custom types get created
-    return `/doc/${doc.id}`
-  }
-
   return (
     <React.Fragment>
       {
         doc ? (
           <div>
-            <h1>{RichText.asText(doc.data.title)}</h1>
-            <RichText render={doc.data.description} linkResolver={linkResolver} />
-            <img alt='cover' src={doc.data.image.url} />
+            <HomePage />
+            <div>
+              <h1>{RichText.asText(doc.data.title)}</h1>
+              <img alt='cover' src={doc.data.image.url} />
+            </div>
           </div>
         ) : <div>No content</div>
       }
+
     </React.Fragment>
   )
 }
